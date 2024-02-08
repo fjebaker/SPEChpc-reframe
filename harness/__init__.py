9ed41b4e915061d23ad2db77cd7ddc09b66d6f5c
fjebaker <fergusbkr@gmail.com>
2024-02-08 13:21:40 +0000

    feat: add builders for all tiny benchmarks

--- harness/__init__.py
+++ harness/__init__.py
@@ -8,6 +8,34 @@ from harness.base import SPEChpcBase
 
 from harness.frequency import FrequencySweepAll, FrequencySweepChosen
 
+# small
 
 class build_Weather_s(build_SPEChpc_benchmark_Base):
     spechpc_benchmark = "635.weather_s"
+
+# tiny
+
+class build_Weather_t(build_SPEChpc_benchmark_Base):
+    spechpc_benchmark = "535.weather_t"
+
+class build_Lbm_t(build_SPEChpc_benchmark_Base):
+    spechpc_benchmark = "505.lbm_t"
+
+class build_Soma_t(build_SPEChpc_benchmark_Base):
+    spechpc_benchmark = "513.soma_t"
+
+class build_Tealeaf_t(build_SPEChpc_benchmark_Base):
+    spechpc_benchmark = "518.tealeaf_t"
+
+class build_Clvleaf_t(build_SPEChpc_benchmark_Base):
+    spechpc_benchmark = "519.clfleaf_t"
+
+class build_Pot3d_t(build_SPEChpc_benchmark_Base):
+    spechpc_benchmark = "528.pot3d_t"
+
+class build_Sph_Exa_t(build_SPEChpc_benchmark_Base):
+    spechpc_benchmark = "532.sph_exa_t"
+
+class build_Hpgmgfv_Exa_t(build_SPEChpc_benchmark_Base):
+    spechpc_benchmark = "534.hpgmgfv_t"
+
