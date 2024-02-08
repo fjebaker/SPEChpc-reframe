e2b630f0b3c25f7915cd16b1b29fe01cd01f5914
fjebaker <fergusbkr@gmail.com>
2024-02-08 11:35:27 +0000

    feat: added frequency lookup table

--- harness/frequency.py
+++ harness/frequency.py
@@ -2,6 +2,64 @@ import reframe as rfm
 import reframe.core.builtins as blt
 import reframe.utility.sanity as sn
 
+F_MHZ = 1000  # hz
+F_GHZ = 1000 * F_MHZ
+
+FREQUENCY_LOOKUP = {
+    "cclake": [
+        2.20 * F_GHZ,
+        2.20 * F_GHZ,
+        2.10 * F_GHZ,
+        2.00 * F_GHZ,
+        1.90 * F_GHZ,
+        1.80 * F_GHZ,
+        1.70 * F_GHZ,
+        1.60 * F_GHZ,
+        1.50 * F_GHZ,
+        1.40 * F_GHZ,
+        1.30 * F_GHZ,
+        1.20 * F_GHZ,
+        1.10 * F_GHZ,
+        1000 * F_MHZ,
+    ],
+    "sapphire": [
+        2.00 * F_GHZ,
+        2.00 * F_GHZ,
+        1.90 * F_GHZ,
+        1.80 * F_GHZ,
+        1.70 * F_GHZ,
+        1.60 * F_GHZ,
+        1.50 * F_GHZ,
+        1.40 * F_GHZ,
+        1.30 * F_GHZ,
+        1.20 * F_GHZ,
+        1.10 * F_GHZ,
+        1000 * F_MHZ,
+        900 * F_MHZ,
+        800 * F_MHZ,
+    ],
+    "icelake": [
+        2.60 * F_GHZ,
+        2.60 * F_GHZ,
+        2.50 * F_GHZ,
+        2.30 * F_GHZ,
+        2.20 * F_GHZ,
+        2.10 * F_GHZ,
+        2.00 * F_GHZ,
+        1.80 * F_GHZ,
+        1.70 * F_GHZ,
+        1.60 * F_GHZ,
+        1.40 * F_GHZ,
+        1.30 * F_GHZ,
+        1.20 * F_GHZ,
+        1.10 * F_GHZ,
+        900 * F_MHZ,
+        800 * F_MHZ,
+    ],
+    # fergus's test system
+    "clusterlaine": [2.0 * F_GHZ, 1.0 * F_GHZ],
+}
+
 
 class FrequencySweep(rfm.RegressionMixin):
 
